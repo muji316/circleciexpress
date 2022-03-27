40fe5791c71b8676dc340665712029986f846308
Syed Mujtaba Ahmed Razvi <97607305+muji316@users.noreply.github.com>
2022-03-27 22:33:49 +0400

    MIT added in string
    Added MIT in comparison string

--- test.js
+++ test.js
@@ -2,15 +2,15 @@ const assert = require("assert");
 const httpMocks = require("node-mocks-http");
 const exampleRouteHandler = require("./example-router");
 describe("Example Test 1", () => {
-  it("should return 'Goodbye Earthling!' for GET /example", () => {
-    const mockRequest = httpMocks.createRequest({
-      method: "GET",
-      url: "/example"
+    it("should return 'Goodbye Earthling MIT!' for GET /example", () => {
+        const mockRequest = httpMocks.createRequest({
+            method: "GET",
+            url: "/example"
+        });
+        const mockResponse = httpMocks.createResponse();
+        exampleRouteHandler(mockRequest, mockResponse);
+        const actualResponseBody = mockResponse._getData();
+        const expectedResponseBody = "Goodbye Earthling MIT!";
+        assert(actualResponseBody, expectedResponseBody);
     });
-    const mockResponse = httpMocks.createResponse();
-    exampleRouteHandler(mockRequest, mockResponse);
-    const actualResponseBody = mockResponse._getData();
-    const expectedResponseBody = "Goodbye Earthling!";
-    assert(actualResponseBody, expectedResponseBody);
-  });
 });
